5b32bc278ee5020eb2ab7a6d0b4dcddf1c97654a
Your Name <nmushtuk@hotmail.com>
2019-03-31 14:31:26 -0600

    fix facial detection and faster video updates

--- facialdetection.py
+++ facialdetection.py
@@ -7,19 +7,19 @@ GPIO.setup(11,GPIO.OUT)
 GPIO.setup(13,GPIO.OUT)
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 cap = cv2.VideoCapture(0)
-faceexists = False
+#faceexists = False
 p1 = GPIO.PWM(11,50)
 p2 = GPIO.PWM(13,50)
 
-cap.set(3,640) # set Width
-cap.set(4,480) # set Height
+cap.set(3,320) # set Width
+cap.set(4,240) # set Height
 
-p1.start(7.5)
-p2.start(7.5)
+#p1.start(7.5)
+#p2.start(7.5)
 
 def sweep():
 	try:
-		while faceexists == False:
+#		while faceexists == False:
 			#p1.ChangeDutyCycle(8.5)
 			#p2.ChangeDutyCycle(8.5)
 			#time.sleep(1)
@@ -41,7 +41,8 @@ def sweep():
 
 
 while True:
-	sweep()
+    #sweep()
+    
     ret, img = cap.read()
     img = cv2.flip(img, -1)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -53,18 +54,18 @@ while True:
     )
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        faceexists = True
-        cv2.circle(img,(320,220),3,(255,0,0),3)
+        #faceexists = True
+        xcen = x +(w/2)
+        ycen = y + (h/2)
+        cv2.circle(img,(xcen,ycen),3,(255,0,0),3)
         roi_gray = gray[y:y+h, x:x+w]
         roi_color = img[y:y+h, x:x+w]  
-        print(x,y)
+
+        print(xcen,ycen)
     cv2.imshow('video',img)
     k = cv2.waitKey(1) & 0xff
     if k == 27: # press 'ESC' to quit
         break
-        
-        
-
         
 cap.release()
 cv2.destroyAllWindows()
